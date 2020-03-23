dfe41ada3af7bca9b685f9ca23b7fc0e3bde5522
flyInTheSky123 <53693690+flyInTheSky123@users.noreply.github.com>
2020-03-23 20:05:38 +0800

    Update second.java

--- src/second.java
+++ src/second.java
@@ -1,5 +1,7 @@
 public class second {
     public static void main(String[] args) {
         System.out.println("second");
+         System.out.println("这是在github上面改的！不是上传而来的！");
+        
     }
 }
